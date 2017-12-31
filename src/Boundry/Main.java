8d7d57d625d766f28ac00a9e5b78eb19deda7fe7
Sagiv.M <Sagiv.M@LAPTOP-Q00RSK5P>
2017-12-31 22:30:50 +0200

    SagivM

---
+++ src/Boundry/Main.java
@@ -0,0 +1,36 @@
+package Boundry ;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+// imports...
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+
+        // just load fxml file and display it in the stage:
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainUI.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    // main method to support non-JavaFX-aware environments:
+
+    public static void main(String[] args) {
+        // starts the FX toolkit, instantiates this class, 
+        // and calls start(...) on the FX Application thread:
+        launch(Main.class,args); 
+    }
+}
