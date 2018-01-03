e208fc8c387ecfc15e48564f17dc4d8f9fc452d4
Sagiv.M <Sagiv.M@10.10.3.102>
2018-01-03 13:08:06 +0200

    SagivM FIN stage 1 and need to test ngrams

--- src/Boundry/Main.java
+++ src/Boundry/Main.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-
 // imports...
 
 public class Main extends Application {
--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,8 +1,6 @@
 package Controller;
 
-import java.awt.Component;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,11 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
 
-import org.apache.pdfbox.cos.COSDocument;
-import org.apache.pdfbox.io.RandomAccessRead;
-import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
-
+import org.apache.pdfbox.text.PDFTextStripper;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -39,8 +34,8 @@ public class MainController implements Initializable {
 	private static int kmax;
 	private static int segmentSize;
 	private static int evolution;
-	private static String doc;
 	private String filePath;
+	private String doc;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -53,25 +48,28 @@ public class MainController implements Initializable {
 		kmax = Integer.parseInt(kMaxTextField.getText());
 		segmentSize = Integer.parseInt(sSizeTextField.getText());
 		evolution = Integer.parseInt(evoTextField.getText());
-
-		//fix pdf analyzer
-		
-		PDDocument pd = new PDDocument();
+		// Extract pdf file text
 		File file = new File(filePath);
+		PDDocument document = PDDocument.load(file);
+		PDFTextStripper pdfTextStripper = new PDFTextStripper();
+		pdfTextStripper.setStartPage(0);
+		pdfTextStripper.setStartPage(document.getNumberOfPages());
+		doc = pdfTextStripper.getText(document);
+		System.out.print(doc);
+		document.close();
+		// Send settings to stem and stop word analyzer
 	}
 
 	@FXML
 	private void browse(ActionEvent event) throws IOException {
-		JFileChooser chooser =new JFileChooser();
-		FileNameExtensionFilter filter=new FileNameExtensionFilter("*","pdf");
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("*", "pdf");
 		chooser.setFileFilter(filter);
-		int returnVal=chooser.showOpenDialog(null);
-		if(returnVal==JFileChooser.APPROVE_OPTION)
-		{
-			filePath=chooser.getSelectedFile().getAbsolutePath();
+		int returnVal = chooser.showOpenDialog(null);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			filePath = chooser.getSelectedFile().getAbsolutePath();
 			fileTextField.setText(filePath);
 		}
-		
 
 	}
 
