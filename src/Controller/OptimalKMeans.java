525bc7c92041eba16e9b8a14944856360e76c289
noyma <noyma@10.10.1.154>
2018-01-25 18:30:52 +0200

    k means

--- src/Controller/OptimalKMeans.java
+++ src/Controller/OptimalKMeans.java
@@ -7,15 +7,13 @@ import Entity.Kmeans;
 public class OptimalKMeans {
 
 	public static ArrayList<Integer>Silhouette;
-	public int findMaxSilhouette (ArrayList<Double> pai, int Kmax) {
+	
+	public ArrayList<Integer> findMaxSilhouette (ArrayList<ArrayList<Double>> pai, int Kmax) {
 		double[][] data=(double[][]) pai.toArray();
-		int max=-1;
 		for (int i=2;i<Kmax;i++) {
 			Kmeans km=new Kmeans(i,data);
 			Silhouette.add((int)km.getAverageSilhouetteValue());
-			if(max<km.getAverageSilhouetteValue())
-				max=(int) km.getAverageSilhouetteValue();
 		}
-		 return max;
+		 return Silhouette;
 	}
 }
